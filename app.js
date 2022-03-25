53a5c5bbe17310406318c41d69e8449b22756d12
nnoranna <anagy.nora.anna@gmail.com>
2022-03-25 13:47:52 +0100

    Connect html to server

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const http = require("http");
+const fs = require("fs");
 const port = 9000;
 
 const server = http.createServer(function(req, res) {
-    res.write("Hello Node")
-    res.end()
+    res.writeHead(200, {"Content-Type": "text/html"})
+    fs.readFile("index.html", function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write("Error: File Not Found")
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
 });
 
 server.listen(port, function(error) {
